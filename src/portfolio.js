8f4322c79e2c361ec89c57078f1c6d01776dd6be
nitishsaikumark <nitishsaikumarkadiyam@gmail.com>
2025-03-05 23:46:55 -0700

    updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Nitish",
   title: "Hi all, I'm Nitish",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs / TypeScript and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with Java / JavaScript / Reactjs / Nodejs / TypeScript and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
